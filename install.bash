c0aba74def53a0b8bfbb13be580a42497dd79f87
carrvo <carrvo@gmail.com>
2024-12-11 19:34:03 -0700

    Revert "third experiment - check compatibility with IndieAuth"
    This reverts commit a7814d4e16634f537aaedc6a6acada22e7d92afb.

--- install.bash
+++
@@ -1,7 +0,0 @@
-sudo apt install composer php-curl php-dom
-composer require indieauth/client
-
-#composer update -W
-#composer update --ignore-platform-req=ext-dom
-#composer install 
-#composer require indieauth/client
