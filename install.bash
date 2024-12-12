a7814d4e16634f537aaedc6a6acada22e7d92afb
carrvo <carrvo@gmail.com>
2024-12-11 19:33:51 -0700

    third experiment - check compatibility with IndieAuth

---
+++ install.bash
@@ -0,0 +1,7 @@
+sudo apt install composer php-curl php-dom
+composer require indieauth/client
+
+#composer update -W
+#composer update --ignore-platform-req=ext-dom
+#composer install 
+#composer require indieauth/client
