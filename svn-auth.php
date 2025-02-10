60868f7ec26a3232132e86d02ddeb5e0f3baff74
carrvo <carrvo@gmail.com>
2025-02-10 16:56:49 -0700

    Revert "Merge pull request #6 from carrvo/bugs/better-error-logging"
    This reverts commit 414517fbb486658866a248e3eaa47f5dec07d524, reversing
    changes made to 82e87ae353e3fd8515cf5c50ce6b8cf8279b24c9.

--- svn-auth.php
+++ svn-auth.php
@@ -31,13 +31,13 @@ $svn_path = preg_replace('/!svn\/rvr\/\d+\//', '', $svn_path);
 // special WebDAV URIs
 // https://svn.apache.org/repos/asf/subversion/trunk/notes/http-and-webdav/http-protocol-v2.txt
 if (str_ends_with($location_path, "/!svn/me")) {
-	error_log("[authnz_external:svn-auth:info] special WebDAV override for $user to create a transaction");
+	fwrite(STDERR, "[authnz_external:svn-auth:info] special WebDAV override for $user to create a transaction\n");
 	exit(0);
 }
 
 // Parent override
 if (count($group_array) > 2 && strcmp($group_array[2], 'ParentIfNotExist') === 0) {
-	error_log("[authnz_external:svn-auth:info] finding parent for $svn_path");
+	fwrite(STDERR, "[authnz_external:svn-auth:info] finding parent for $svn_path\n");
 	$cmd = "svn list 'file://$svn_path'";
 	$output=null;
 	$retval=null;
@@ -46,14 +46,14 @@ if (count($group_array) > 2 && strcmp($group_array[2], 'ParentIfNotExist') === 0
 		$parent_pos = strrpos($svn_path, '/', 1); // exclude final slash (/) if child is folder
 		if ($parent_pos !== false) {
 			$svn_path = substr($svn_path, 0, $parent_pos);
-			error_log("[authnz_external:svn-auth:info] did not exist, overridden with parent $svn_path for permissions");
+			fwrite(STDERR, "[authnz_external:svn-auth:info] did not exist, overridden with parent $svn_path for permissions\n");
 		} else {
 			break;
 		}
 		$cmd = "svn list 'file://$svn_path'";
 		$cmd_ran = exec($cmd, $output, $retval);
 	}
-	error_log("[authnz_external:svn-auth:info] done with parent, now using $svn_path");
+	fwrite(STDERR, "[authnz_external:svn-auth:info] done with parent, now using $svn_path\n");
 }
 
 // Permissions
